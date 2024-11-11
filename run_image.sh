7ce255cea8532c042e3b9892f31ef05f91db5eb7
fsm <fsm>
2024-11-11 14:37:50 +0530

    adjusted run_image.sh based on fsm system

--- run_image.sh
+++ run_image.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 xhost +local:docker
-    docker run --rm -it --net=host --name piston_container --volume /tmp/.X11-unix:/tmp/.X11-unix --volume /home/anurag/Desktop/code/Piston_fsm:/fsm_piston --workdir /fsm_piston --env DISPLAY=$DISPLAY --env ROS_MASTER_URI=http://localhost:11311/ --privileged -v /dev/bus/usb:/dev/bus/usb --env ROS_HOSTNAME=192.168.0.101 --env ROS_IP=192.168.0.100 mr_fsm  /bin/bash 
+    docker run --rm -it --net=host --name piston_container --volume /tmp/.X11-unix:/tmp/.X11-unix --volume /home/dell/cobot_ws/:/fsm_piston --workdir /fsm_piston --env DISPLAY=$DISPLAY --env ROS_MASTER_URI=http://localhost:11311/ --privileged -v /dev/bus/usb:/dev/bus/usb --env ROS_HOSTNAME=192.168.0.101 --env ROS_IP=192.168.0.100 anuragch/ubuntu20  /bin/bash 
